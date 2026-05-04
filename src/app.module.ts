df4887bdbb2cfd8e146d2d0ad762c3d1a8fa3fc7
Gustavo Paulucci <gpaulucci@gmail.com>
2026-05-04 15:04:35 -0300

    changes to mvp v.1.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { 
+  KeycloakConnectModule, 
+  ResourceGuard, 
+  RoleGuard, 
+  AuthGuard 
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule, // <--- Debe estar aquí
+    KeycloakConnectModule.register({
+      authServerUrl: process.env.KEYCLOAK_AUTH_SERVER_URL, // http://keycloak:8080
+      realm: process.env.KEYCLOAK_REALM,                  // Quasar_Solutions
+      clientId: process.env.KEYCLOAK_CLIENT_ID,          // ms-auth-backend
+      secret: process.env.KEYCLOAK_CLIENT_SECRET,        // El que sacaste de la UI
+      // Esto es clave para que funcione dentro de Docker
+      policyEnforcer: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE, 
+    }),
+  ],
+  providers: [
+    // Estos Guards protegen TODA la aplicación por defecto
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_GUARD, useClass: ResourceGuard },
+    { provide: APP_GUARD, useClass: RoleGuard },
   ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public, Roles } from 'nest-keycloak-connect';
 
 @Controller('auth') // <--- Esto define la base: /auth
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Get('login') // <--- Esto define el endpoint: /auth/login
+  @Public()
   login(@Res() res) {
     const url = this.authService.getAuthorizationUrl();
     return res.redirect(url);
+  }
+
+  @Get('privado')
+  @Roles({ roles: ['admin'] }) // Solo usuarios con rol 'admin'
+  findOneSafe() {
+    return 'Solo tú puedes ver esto si tienes el token y el rol';
   }
 
   // Asegúrate de tener también el callback para que Keycloak pueda volver
